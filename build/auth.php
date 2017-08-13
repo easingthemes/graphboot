255eac6f1a07bbfb0ab0a1088a73505f07ac6cca
Dragan Filipovic <dragan.filipovic@netcentric.biz>
2017-08-13 23:58:28 +0200

    php

--- build/auth.php
+++ build/auth.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
-  <meta name="theme-color" content="#000000">
-  <link rel="manifest" href="/manifest.json">
-  <link rel="shortcut icon" href="/favicon.ico">
-  <title>Badges</title>
-  <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
-  <link href="/static/css/main.685015e3.css" rel="stylesheet">
-</head>
-<body>
-<noscript>You need to enable JavaScript to run this app.</noscript>
 <?php
+define('YOUR_APP_NAME', 'graphboot');
 define('OAUTH2_CLIENT_ID', 'f30606c2ad5e4a14afe7');
 define('OAUTH2_CLIENT_SECRET', '87484dd0b03d7c49d35c15c054e74eb4b0484b86');
 
@@ -61,14 +48,38 @@ if(get('code')) {
   header('Location: ' . $_SERVER['PHP_SELF']);
 }
 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+  <meta name="theme-color" content="#000000">
+  <link rel="manifest" href="/manifest.json">
+  <link rel="shortcut icon" href="/favicon.ico">
+  <title>Badges</title>
+  <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+  <link href="/static/css/main.685015e3.css" rel="stylesheet">
+</head>
+<body>
+<noscript>You need to enable JavaScript to run this app.</noscript>
+<?php
+
 if(session('access_token')) {
   $user = apiRequest($apiURLBase . 'user');
+  $graph = apiRequest('https://github.com/users/easingthemes/contributions');
 
   echo '<h3>Logged In</h3>';
   echo '<h4>' . $user->name . '</h4>';
   echo '<pre>';
   print_r($user);
   echo '</pre>';
+  echo '<pre>';
+  print_r($graph);
+  echo '</pre>';
+  echo '<div id="root"></div>';
+  echo '<script type="text/javascript" src="/static/js/main.80b7ae55.js"></script>';
 
 } else {
   echo '<h3>Not logged in</h3>';
@@ -88,6 +99,7 @@ function apiRequest($url, $post=FALSE, $headers=array()) {
   if(session('access_token'))
     $headers[] = 'Authorization: Bearer ' . session('access_token');
 
+  $headers[] = 'User-Agent: YOUR_APP_NAME';
   curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
   $response = curl_exec($ch);
@@ -102,7 +114,6 @@ function session($key, $default=NULL) {
   return array_key_exists($key, $_SESSION) ? $_SESSION[$key] : $default;
 }
 ?>
-<div id="root"></div>
-<script type="text/javascript" src="/static/js/main.80b7ae55.js"></script>
+end
 </body>
 </html>
