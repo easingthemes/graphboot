20efb50a11bf67d7c45bd76c22f69e7ccfeb5e6e
Dragan Filipovic <dragan.filipovic@netcentric.biz>
2017-08-24 03:12:59 +0200

    php

--- src/containers/Calendar/constants.js
+++ src/containers/Calendar/constants.js
@@ -2,4 +2,4 @@ export const DEFAULT_ACTION = 'containers/Calendar/DEFAULT_ACTION';
 export const SET_DAYS = 'containers/Calendar/SET_DAYS';
 export const GET_DAYS = 'containers/Calendar/GET_DAYS';
 export const GET_CONTRIBUTIONS_DOM = 'containers/Calendar/GET_CONTRIBUTIONS_DOM';
-export const API_URL = '/contributions';
+export const API_URL = '/contributions/';
